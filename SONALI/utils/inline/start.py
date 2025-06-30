3456b29f1df8b888856a45ebae13523b5a006028
Samarth-1206 <pawar06samarth14@gmail.com>
2025-06-30 19:18:42 +0530

    Update start.py

--- SONALI/utils/inline/start.py
+++ SONALI/utils/inline/start.py
@@ -26,7 +26,7 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"],  url=f"tg://user?id={config.Esna8ense8}"),
+            InlineKeyboardButton(text=_["S_B_5"],  url=f"t.me/Esna8ense8"),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
